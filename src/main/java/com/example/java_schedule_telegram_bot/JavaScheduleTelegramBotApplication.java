bf6122439a9b336faf6866cbd76fcf97f183d53a
BelkaAlyenka <alakrinskaaalena@gmail.com>
2026-08-01 19:47:59 +0700

    First commit

---
+++ src/main/java/com/example/java_schedule_telegram_bot/JavaScheduleTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_schedule_telegram_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaScheduleTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaScheduleTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_schedule_telegram_bot/JavaScheduleTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_schedule_telegram_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaScheduleTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
